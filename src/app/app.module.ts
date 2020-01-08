de14bbb015d41bf6b7d1736ce6a2860b7ffe1d01
Vairav Lakshmanan <vairavmahi@gmail.com>
2020-01-08 12:35:43 +0530

    chore: Router: Add router to the app
    Signed-off-by: Vairav Lakshmanan <vairavmahi@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './components/app/app.component';
 import { metaReducers, reducers } from './modules/reducers';
+
+const appRoutes: Routes = [
+  {
+    path: 'main',
+    component: AppComponent,
+  },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: '**', component: AppComponent },
+];
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,6 +28,7 @@ import { metaReducers, reducers } from './modules/reducers';
         strictActionImmutability: true,
       },
     }),
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent],
