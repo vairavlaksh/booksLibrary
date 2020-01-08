2c4778e1b1670ad65aab3675ecf0dd56b44f4933
Vairav Lakshmanan <vairavmahi@gmail.com>
2020-01-08 16:32:40 +0530

    chore: ngrx: Add appropriate packages
    Signed-off-by: Vairav Lakshmanan <vairavmahi@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './components/app/app.component';
-import { metaReducers, reducers } from './modules/reducers';
 import { MainComponent } from './components/main/main.component';
+import { metaReducers, reducers } from './modules/reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 const appRoutes: Routes = [
   {
     path: 'main',
-    component: AppComponent,
+    component: MainComponent,
   },
   { path: '', redirectTo: '/main', pathMatch: 'full' },
-  { path: '**', component: AppComponent },
+  { path: '**', component: MainComponent },
 ];
 
 @NgModule({
@@ -30,6 +35,9 @@ const appRoutes: Routes = [
       },
     }),
     RouterModule.forRoot(appRoutes),
+    EffectsModule.forRoot([AppEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
