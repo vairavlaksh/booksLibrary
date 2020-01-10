3c0131d475bad208133573d1d9b36cba4a522fff
Vairav Lakshmanan <vairavmahi@gmail.com>
2020-01-10 22:21:20 +0530

    feat: header: Add logo and home icon
    Signed-off-by: Vairav Lakshmanan <vairavmahi@gmail.com>

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  isNotHomePage = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {
+    router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe(value => {
+        if (value['url'] !== '/books') {
+          this.isNotHomePage = true;
+        } else {
+          this.isNotHomePage = false;
+        }
+      });
   }
 
+  ngOnInit() {}
 }
