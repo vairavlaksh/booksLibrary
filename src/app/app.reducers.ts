bdf8d50be9f8dbcc267b9fe97bb24663f18410fc
Vairav Lakshmanan <vairavmahi@gmail.com>
2020-01-10 22:16:56 +0530

    feat: redux(app): Accomodate changes due to deletion
    Signed-off-by: Vairav Lakshmanan <vairavmahi@gmail.com>

--- src/app/app.reducers.ts
+++ src/app/app.reducers.ts
@@ -1,10 +1,8 @@
 import { ActionReducerMap } from '@ngrx/store';
 
 import { AppState } from './app.state';
-import { bookReducers } from './modules/book/book.reducers';
 import { booksReducers } from './modules/books/books.reducers';
 
 export const appReducers: ActionReducerMap<AppState, any> = {
   books: booksReducers,
-  selectedBook: bookReducers,
 };
--- src/app/app.state.ts
+++ src/app/app.state.ts
@@ -1,14 +1,11 @@
-import { BookState, initialBookState } from './modules/book/book.state';
 import { BooksState, initialBooksState } from './modules/books/books.state';
 
 export interface AppState {
   books: BooksState;
-  selectedBook: BookState;
 }
 
 export const initialAppState: AppState = {
   books: initialBooksState,
-  selectedBook: initialBookState,
 };
 
 export const getInitialBooksState = () => initialAppState;
